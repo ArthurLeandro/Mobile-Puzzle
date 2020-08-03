5b78383d4e1db7fc3e8c1054ce754c1c811804bc
Paper Board Studios <paperboardstudios@gmail.com>
2020-08-03 20:35:48 -0300

    Projeto finalizado

---
+++ Assets/Scripts/lightSwitch.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightSwitch : MonoBehaviour {
+
+
+	public bool isOn = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void change(){
+
+		Vector3 curPos = this.transform.Find ("Plane").position;
+
+		if(isOn){
+
+			isOn = false;
+			this.transform.Find("Plane").position = new Vector3(curPos.x,curPos.y,curPos.z +.5f);
+			//this.transform.localEulerAngles = new Vector3(0,45,0);
+		
+		}else{
+			isOn = true;
+			this.transform.Find("Plane").position = new Vector3(curPos.x,curPos.y,curPos.z -.5f);
+			//this.transform.localEulerAngles = Vector3.zero;
+		}
+	}
+
+
+
+}
---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class menu : MonoBehaviour {
+
+	private int anim1 = Animator.StringToHash("mainState");
+	private int anim2 = Animator.StringToHash("hudState");
+
+	private Animator animator1;
+	private Animator animator2;
+
+	// Use this for initialization
+	void Start () {
+
+		animator1 = this.transform.Find ("Main").GetComponent<Animator> ();
+		animator2 = this.transform.Find ("Hud").GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setLevelText(string name){
+		this.transform.Find ("Hud").transform.Find ("headline").GetComponent<Text> ().text = "LVL " + name;
+	}
+
+
+	public void animateMenu(){
+		animator1.SetInteger (anim1, 1);
+		animator2.SetInteger (anim2, 1);
+	}
+
+
+}
